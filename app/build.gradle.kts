46496a432b6e3a655a29d820b767955efe118340
Mohamed Eldirdiri <mohamedeldirdiri@Mohameds-MacBook-Air.local>
2026-05-04 04:23:57 -0400

    Add Android wrapper and build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,15 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
